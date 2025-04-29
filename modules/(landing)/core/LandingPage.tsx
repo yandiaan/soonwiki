c8f81bdf02c7b0c83fbd5169a7abe9523cc30566
yandiaan <diansetiawan2121@gmail.com>
2025-04-30 04:39:10 +0700

    fix(landing): update Highlight component colors for better visibility

--- modules/(landing)/core/LandingPage.tsx
+++ modules/(landing)/core/LandingPage.tsx
@@ -45,15 +45,15 @@ const LandingPage = () => {
             duration={0.2}
             gradient={{
               dark: {
-                from: "white",
-                to: "white",
+                from: "#ED6C30",
+                to: "#ED6C30",
               },
               light: {
                 from: "#ED6C30",
                 to: "#ED6C30",
               },
             }}
-            className="text-white font-heading"
+            className="text-white font-heading bg-[#ED6C30] px-3"
           >
             SOON
           </Highlight>
